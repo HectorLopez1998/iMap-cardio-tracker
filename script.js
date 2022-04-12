9fe1b0e2a76918295db9aa5fab79a46f754bcaaf
hector <hectorlopezp1998@gmail.com>
2022-04-12 00:56:47 -0400

    dislpay map

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+/* Checking if the browser supports geolocation and if it does, it will get the current position of the
+user. */
+// takes two functions, one for success and another one for error
+if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function (position) {
+  const { latitude } = position.coords
+  const { longitude } = position.coords
+  const map = L.map('map').setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+}, function () {
+  alert(`Could not get your position`)
+})
+
+console.log(a);
